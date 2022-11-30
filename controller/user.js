720620b1f4023a5d3600ea509757490b35f5bf18
Ammat Ullah Khan ZuberI <57541785+ammatzuberi@users.noreply.github.com>
2022-11-30 10:43:09 +0530

    30-11-2022 commit

--- controller/user.js
+++ controller/user.js
@@ -21,9 +21,7 @@ export const login = (req, res) => {
     var requiredParams = ["email", "password"];
 
     if (!validateRequestBody(keys, requiredParams)) {
-      res
-        .status(400)
-        .json({ error: "Request body should contain - email and password" });
+      res.status(400).json({ error: "Request body should contain - email and password" });
     } else if (!validateEmail(email)) {
       res.status(400).json({ error: "Invalid email address" });
     } else {
@@ -39,40 +37,32 @@ export const login = (req, res) => {
             return acc.email === email;
           });
           console.log(verifiedUser);
-          if (!verifiedUser)
+          if (!verifiedUser){
             res.status(401).json({ error: "User doesnt exist" });
-          bcrypt.compare(
-            password,
-            verifiedUser.password,
-            async function (err, verify) {
+          } else{
+            bcrypt.compare(password, verifiedUser?.password, async function (err, verify) {
               if (err) {
                 console.log(err);
               }
               if (!verify) {
                 res.status(401).json({ error: "Incorrect password" });
               } else {
-                var token = jwt.sign(
-                  { email: email, role: verifiedUser.role },
-                  secret,
-                  {
-                    expiresIn: parseInt(3600),
-                  }
-                );
-
+                var token = jwt.sign({ email: email, role: verifiedUser.role }, secret, {
+                  expiresIn: parseInt(3600),
+                });
+  
                 fs.readFile("./data/data.json", (err, data) => {
                   let userData = {};
                   console.log("file Data" + data.toString());
                   if (data.toString() !== "") {
-                    userData =
-                      JSON.parse(data.toString())?.find(
-                        (data) => data?.email === email
-                      ) || {};
+                    userData = JSON.parse(data.toString())?.find((data) => data?.email === email) || {};
                   }
                   console.log(userData);
                   if (!Object.keys(userData).length && err) {
                     res.status(200).json({
                       msg: "Logged in successfully",
                       token: token,
+                      email,
                       userData: {},
                       role: verifiedUser.role,
                     });
@@ -80,14 +70,16 @@ export const login = (req, res) => {
                     res.status(200).json({
                       msg: "Logged in successfully",
                       token: token,
+                      email,
                       userData,
                       role: verifiedUser.role,
                     });
                   }
                 });
               }
-            }
-          );
+            });
+          }
+          
         }
       });
     }
@@ -110,9 +102,7 @@ export const register = (req, res) => {
     var requiredParams = ["email", "password"];
 
     if (!validateRequestBody(keys, requiredParams)) {
-      res
-        .status(400)
-        .json({ error: "Request body should contain - email and password" });
+      res.status(400).json({ error: "Request body should contain - email and password" });
     } else if (!validateEmail(email)) {
       res.status(400).json({ error: "Invalid email address" });
     } else {
@@ -132,16 +122,12 @@ export const register = (req, res) => {
             bcrypt.hash(password, 10).then(function (hash) {
               users.push({ email, password: hash, role: "user" });
               console.log(users);
-              fs.writeFile(
-                "./data/users.json",
-                JSON.stringify(users),
-                (err) => {
-                  if (err) console.log(err);
-                  res.status(200).json({
-                    msg: "Signup Successfully",
-                  });
-                }
-              );
+              fs.writeFile("./data/users.json", JSON.stringify(users), (err) => {
+                if (err) console.log(err);
+                res.status(200).json({
+                  msg: "Signup Successfully",
+                });
+              });
             });
           }
         }
--- postmark.js
+++ postmark.js
@@ -1,13 +1,20 @@
 var postmark = require("postmark");
-require('dotenv').config()
+require("dotenv").config();
 
-const API_KEY = process.env.POSTMARK_API_KEY 
-console.log(API_KEY)
+const API_KEY = process.env.POSTMARK_API_KEY;
+console.log(API_KEY);
 var client = new postmark.ServerClient(API_KEY);
 
 client.sendEmail({
-  "From": "support@enggenv.com",
-  "To": "recipient@example.com",
-  "Subject": "Test",
-  "TextBody": "Hello from Postmark!"
-});
+  From: "support@enggenv.com",
+  To: "recipient@example.com",
+  Subject: "Test",  
+  TextBody: "Hello from Postmark!",
+}),
+  client.sendEmailBatch(messages, function (error, batchResults) {
+    if (error) {
+      console.error("Unable to send via postmark: " + error.message);
+      return;
+    }
+    console.info("Messages sent to By PostMark");
+  });
